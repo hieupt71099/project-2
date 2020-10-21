8aea32e2be9e28da510714e806b417789b100ae2
Phạm Trung Hiếu <hieupt71099@gmail.com>
2020-10-22 00:21:55 +0700

    upate 12:21

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def about():
 def product():
     return render_template('product.html')
 
+@app.route('/tintuc')
+def tintuc():
+    return render_template('tintuc.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug= True)
