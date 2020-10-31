6e3d5d1d689895ee6741b4382f54f589438ea29d
tranvanthanh99 <thanhvantran99@gmail.com>
2020-10-31 09:49:19 +0700

    update: login register

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import *
+from models.user import User
+import service
+import db
+
 app = Flask(__name__)
+app.secret_key = "suachua"
+db.connect()
 
 @app.route('/')
 def index():
@@ -16,6 +22,58 @@ def product():
 @app.route('/tintuc')
 def tintuc():
     return render_template('tintuc.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json['email']
+    password = request.json['password']
+    
+    res = service.validate_user(email, password)
+  
+    if res:
+        session['user-loggedin'] = True
+        session["user_id"] = str(res.id)
+        session["user-name"] = res.lastname + " " + res.firstname
+        return jsonify({
+            'success': True,
+            'user_id':str(res.id),
+            'email':res.email, 
+            'role':res.role,
+            'firstname':res.firstname, 
+            'lastname':res.lastname, 
+            'phone':res.phone
+        }) , 200
+    else: 
+        return jsonify({'success': False}), 200
+
+@app.route('/logout')
+def logout():
+    if session['user-loggedin'] == False:
+        return redirect(url_for('index'))
+    else:
+        session['user-loggedin'] = False
+        del session["user_id"]
+        del session["user-name"]
+        return redirect(url_for('index'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.json['email']
+    password = request.json['password']
+    firstname = request.json['firstname']
+    lastname = request.json['lastname']
+    phone = request.json['phone']
+
+    res = service.create_user(email, password, firstname, lastname, phone)
+    if res:
+        return jsonify({
+        'success': True
+        }) , 200
+    else: 
+        return jsonify({
+        'success': False,
+        'error' : 'invalid email'
+        }), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug= True)
