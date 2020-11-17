2e345716076aa32072b543d5c52a8dabfe94ca30
Phạm Trung Hiếu <hieupt71099@gmail.com>
2020-11-17 19:36:31 +0700

    khoi tao

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import mongoengine
 
-host = 'mongodb+srv://admin:admin@thanhcluster-soioj.mongodb.net/project-hieu?retryWrites=true&w=majority'
+host = 'mongodb://admin:admin@cluster0-shard-00-00.xnyh2.mongodb.net:27017,cluster0-shard-00-01.xnyh2.mongodb.net:27017,cluster0-shard-00-02.xnyh2.mongodb.net:27017/project-hieu?ssl=true&replicaSet=atlas-ctdkuq-shard-0&authSource=admin&retryWrites=true&w=majority'
 db_name = "project-hieu"
 user_name = "admin"
 password = "admin"
@@ -9,7 +9,7 @@ password = "admin"
 def connect():
     mongoengine.connect(
         db_name, 
-        host='mongodb+srv://admin:admin@thanhcluster-soioj.mongodb.net/project-hieu?retryWrites=true&w=majority',
+        host='mongodb://admin:admin@cluster0-shard-00-00.xnyh2.mongodb.net:27017,cluster0-shard-00-01.xnyh2.mongodb.net:27017,cluster0-shard-00-02.xnyh2.mongodb.net:27017/project-hieu?ssl=true&replicaSet=atlas-ctdkuq-shard-0&authSource=admin&retryWrites=true&w=majority',
         username='admin',
         password='admin'
     )
